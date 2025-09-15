96043f92a0f89ae0794fc4a76fe692f50638c8a3
Carey McLelland <careymclelland@gmail.com>
2025-09-15 14:02:24 -0400

    bump.

--- themes/amateur/neovim.lua
+++ themes/amateur/neovim.lua
@@ -1,11 +1,8 @@
--- This file sets the colorscheme for Neovim.
--- Please note that you need to have a Neovim colorscheme named "amateur"
--- installed for this to work.
 return {
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "amateur",
+			colorscheme = "tokyonight",
 		},
 	},
 }
