f9fded48f414c78496ea8f8b73ff5ef60c24cb32
Carey McLelland <careymclelland@gmail.com>
2025-10-15 10:25:16 -0400

    bump.

---
+++ config/nvim/lua/plugins/snacks-animated-scrolling.lua
@@ -0,0 +1,9 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		scroll = {
+			enabled = true,
+			-- enabled = false, -- Disable scrolling animations
+		},
+	},
+}
---
+++ config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,2 @@
+-- back to horizontal which-key. Vertical sucks, and is for dorks.
+return { "folke/which-key.nvim", opts = { preset = "classic" } }
